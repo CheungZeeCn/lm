e2eb16e1c631bb8f3f53aef21a44b68f3bce4aa8
cheungzeecn <cheungzeecn@gmail.com>
2022-10-06 14:50:51 +0800

    add

--- cn_pretrain_dataset_preprocess.py
+++ cn_pretrain_dataset_preprocess.py
@@ -11,6 +11,27 @@ from libs.lm.datasets import utils as ds_utils
 from libs import utils
 
 
+def pa_preprocessed_format_to_layoutlm_format(lines, fn=""):
+    ret = []
+    for ori_line in lines:
+        line = ori_line.strip()
+        ls = ori_line.split("\t")
+        if line != "":
+            if len(ls) != 9:
+                logging.warning("LINE FORMAT ERROR, IGNORE: [{}]:[{}]".format(fn, line))
+                continue
+            else:
+                char_scaled_bbox = [int(ls[0]), int(ls[1]), int(ls[4]), int(ls[5])]
+                ch = ls[9]
+                chars_and_coords = [{"box": char_scaled_bbox, "text": ch}]
+                scaled_chars_and_coords = ds_utils.rectify_chars_coords(chars_and_coords)
+                for char_info in scaled_chars_and_coords:
+                    bbox_string = "\t".join([str(x) for x in char_info['box']])
+                    char_str = "{}\t{}".format(bbox_string, char_info['text'])
+                    ret.append(char_str)
+    return ret
+
+
 def paddle_format_to_layoutlm_format(lines, fn=""):
     x_size, y_size = [float(digit_str) for digit_str in lines[0].strip().split("\t")]
     ret = []
@@ -77,6 +98,7 @@ def seg_cn_file(fn, out_fn, max_len=510):
             fw_p.write(ori_line)
         logging.info("write segmented data(chunk size: {}) to file {} DONE".format(max_len, out_fn))
 
+
 def paddle_ocr_to_cn_dataset(dir_path, output_fn):
     """
         paddle ocr format:  文件首行是文件 x, y 大小
@@ -115,9 +137,42 @@ def paddle_ocr_to_cn_dataset(dir_path, output_fn):
     logging.info("write to tmp file: {} DONE".format(tmp_output_fn))
 
 
+def pa_preprocessed_ocr_to_cn_dataset(dir_path, output_fn):
+    """
+         pa_preprocessed_ocr format:  每一行就是一个字
+            从左上角开始，顺时针(x,y) 坐标一共8个已经缩放的坐标字段， 算上识别出来的字 最后有9个字段;
+        layoutlm format: 左上， 右下 两个点坐标 共四个字段
+        注意: 无数据格式验证 要保证输入符合格式要求
+    :param dir_path:
+    :return:
+    """
+    logging.info("paddle_ocr_to_cn_dataset: {} {}".format(dir_path, output_fn))
+    full_fns = ds_utils.collect_files(dir_path, suffix='.txt')
+    logging.info("all {} files".format(len(full_fns)))
+    tmp_output_fn = output_fn + '.tmp'
+    i = 0
+    with open(tmp_output_fn, 'w') as fw:
+        for fn in tqdm.tqdm(full_fns):
+            i += 1
+            # for debug only
+            if i != i:
+                break
+            try:
+                with open(fn, encoding='utf-8') as f:
+                    lines = f.readlines()
+                    lines = pa_preprocessed_format_to_layoutlm_format(lines, fn)
+                    if len(lines) != 0:
+                        fw.write("\n".join(lines) + "\n")
+                fw.write("\n")
+            except Exception as e:
+                logging.info("ERROR in file: [{}], {}, IGNORE".format(fn, e))
+    logging.info("write to tmp file: {} DONE".format(tmp_output_fn))
+
+
 if __name__ == '__main__':
     dir_path = '/home/ana/data2/datasets/layout/大赛1000训练用数据集_ocr'
     output_fn = os.path.join(kp_setup.data_dir, 'datasets', 'cn_pretrain', 'preprocess.txt')
     utils.make_sure_dir_there(os.path.dirname(output_fn))
-    paddle_ocr_to_cn_dataset(dir_path, output_fn)
+    # paddle_ocr_to_cn_dataset(dir_path, output_fn)
+    pa_preprocessed_ocr_to_cn_dataset(dir_path, output_fn)
     seg_cn_file(output_fn + '.tmp', output_fn)
